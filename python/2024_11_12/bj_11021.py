fb40acd35a6dc1f8dc4a87cfb50df67a52636580
allenkang02 <ksw8841@knou.ac.kr>
2024-11-12 10:00:36 +0900

    upload : 2024.11.12. directory files.

---
+++ python/2024_11_12/bj_11021.py
@@ -0,0 +1,10 @@
+# 문제 링크 (주석) : https://www.acmicpc.net/problem/11021
+# 간단한 문제 설명 : 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+# 해결 방법 설명 : 
+
+# # T = count_test_case 
+# A, B = map(int, input().split(" "))
+
+
+
+# print(f"{A+B}")
