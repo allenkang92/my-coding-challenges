2a8145b077ba8550326025a69250476d82843af6
allenkang92 <ksw8841@knou.ac.kr>
2025-01-17 16:44:19 +0900

    upload : sql test.

---
+++ sql/IS_NULL/Lv.1/pg_59407.sql
@@ -0,0 +1,30 @@
+-- 문제 링크 (주석) : https://school.programmers.co.kr/learn/courses/30/lessons/59407?language=oracle
+-- 간단한 문제 설명 : 
+--   동물 보호소에 들어온 동물 중 이름이 있는 동물의 ID를 조회합니다.
+--
+-- 테이블 스키마 :
+--   - 테이블명 : ANIMAL_INS
+--   - 컬럼정보:
+--       ANIMAL_ID VARCHAR(N)    NOT NULL  -- 동물 아이디
+--       ANIMAL_TYPE VARCHAR(N)  NOT NULL  -- 생물 종
+--       DATETIME DATETIME       NOT NULL  -- 보호 시작일
+--       INTAKE_CONDITION VARCHAR(N) NOT NULL  -- 보호 시작시 상태
+--       NAME VARCHAR(N)         TRUE      -- 이름
+--       SEX_UPON_INTAKE VARCHAR(N) NOT NULL  -- 성별 및 중성화 여부
+--
+-- 해결 방법 설명 :
+--   1. WHERE절에서 NAME IS NOT NULL 조건으로 이름이 있는 동물 필터링
+--   2. ORDER BY로 ANIMAL_ID 오름차순 정렬
+--
+-- 시간/공간 복잡도 :
+--   - 시간복잡도 : O(N log N) - 정렬로 인해
+--   - 공간복잡도 : O(N) - 결과 저장 공간
+
+SELECT 
+    ANIMAL_ID  -- 동물 아이디만 조회
+FROM 
+    ANIMAL_INS
+WHERE 
+    NAME IS NOT NULL  -- 이름이 NULL이 아닌 동물만 선택
+ORDER BY 
+    ANIMAL_ID ASC;  -- 동물 아이디 기준 오름차순 정렬
---
+++ sql/STRING_DATE/pg_157343.sql
@@ -0,0 +1,41 @@
+-- 문제 링크 (주석) : https://school.programmers.co.kr/learn/courses/30/lessons/157343?language=oracle
+-- 간단한 문제 설명 : 
+--   자동차 대여 회사의 자동차 중 네비게이션 옵션이 포함된 차량을 조회합니다.
+--
+-- 테이블 스키마 :
+--   - 테이블명 : CAR_RENTAL_COMPANY_CAR
+--   - 컬럼정보:
+--       CAR_ID INTEGER        NOT NULL  -- 자동차 ID
+--       CAR_TYPE VARCHAR(255) NOT NULL  -- 자동차 종류
+--       DAILY_FEE INTEGER     NOT NULL  -- 일일 대여 요금
+--       OPTIONS VARCHAR(255)  NOT NULL  -- 옵션 리스트 (콤마로 구분)
+
+-- 방법 1: LIKE 연산자 사용
+-- 장점: 직관적이고 가독성이 좋음
+-- 단점: 와일드카드(%) 사용으로 인덱스 활용이 제한적
+SELECT 
+    CAR_ID, 
+    CAR_TYPE, 
+    DAILY_FEE, 
+    OPTIONS
+FROM 
+    CAR_RENTAL_COMPANY_CAR
+WHERE 
+    OPTIONS LIKE '%네비게이션%'
+ORDER BY 
+    CAR_ID DESC;
+
+-- 방법 2: INSTR 함수 사용
+-- 장점: 성능상 더 효율적일 수 있음
+-- 단점: 가독성이 LIKE에 비해 떨어질 수 있음
+SELECT 
+    CAR_ID, 
+    CAR_TYPE, 
+    DAILY_FEE, 
+    OPTIONS
+FROM 
+    CAR_RENTAL_COMPANY_CAR
+WHERE 
+    INSTR(OPTIONS, '네비게이션') > 0
+ORDER BY 
+    CAR_ID DESC;
