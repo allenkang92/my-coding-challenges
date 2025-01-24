3da929bc159a5c52d28a2e13d0cfbbb537057a4f
allenkang92 <ksw8841@knou.ac.kr>
2025-01-24 17:20:47 +0900

    upload : files. not complete.

---
+++ sql/GROUP_BY/Lv.4/(미완료)pg_59413.sql
@@ -0,0 +1,19 @@
+-- 문제 링크 (주석) : https://school.programmers.co.kr/learn/courses/30/lessons/59413
+-- 간단한 문제 설명 : 
+-- 테이블 스키마 :
+--   - 테이블명 : 
+--   - 컬럼명 및 타입 : 
+-- 해결 방법 설명 :
+--   1. 쿼리 작성 단계
+--   2. 사용한 SQL 문법 및 함수 설명
+--   3. 쿼리 최적화 방법 (인덱스 사용, 조인 방식 등)
+-- 시간/공간 복잡도 :
+--   - 시간복잡도 : 
+--   - 공간복잡도 : 
+
+-- SELECT TO_NUMBER(DATETIME, 'HH24')AS HOUR, COUNT
+SELECT TO_CHAR(DATETIME, 'HH24') AS HOUR, (SELECT COUNT(TO_CHAR(DATETIME, 'HH24')) FROM ANIMAL_OUTS) AS COUNT
+FROM ANIMAL_OUTS
+-- GROUP BY TO_CHAR(DATETIME, 'HH24')
+ORDER BY HOUR ASC
+;
