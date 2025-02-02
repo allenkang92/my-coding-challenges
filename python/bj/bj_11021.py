1d0188aea60f900a502f4415fd5b83e80eccb827
allenkang92 <ksw8841@knou.ac.kr>
2025-02-02 15:28:55 +0900

    upload : python test.

--- python/bj/bj_11021.py
+++ python/bj/bj_11021.py
@@ -1,11 +1,24 @@
 # 문제 링크 (주석) : https://www.acmicpc.net/problem/11021
-# 간단한 문제 설명 : 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+# 간단한 문제 설명 : 
+#   테스트 케이스마다 두 정수 A와 B를 입력받아 A+B를 출력
+#   각 케이스는 "Case #x: " 형식으로 출력
+#
 # 해결 방법 설명 : 
+#   - T개의 테스트 케이스 수를 입력받음
+#   - 각 케이스마다:
+#     - A, B를 입력받아 합을 계산
+#     - Case 번호와 함께 결과 출력
+#
+# 시간/공간 복잡도 : 
+#   - 시간 복잡도: O(T)
+#   - 공간 복잡도: O(1)
 
-# # T = count_test_case 
-# A, B = map(int, input().split(" "))
+T = int(input())
 
+case_number = 0
 
-
-# print(f"{A+B}")
-
+for _ in range(T):
+    A, B = map(int, input().split(" "))
+    
+    print(f"Case #{case_number+1}: ", A+B)
+    case_number += 1
