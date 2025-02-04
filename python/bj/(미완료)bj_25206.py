8dee423a8c45a63c837169ebd0df7eebd092855f
allenkang92 <ksw8841@knou.ac.kr>
2025-02-05 00:25:32 +0900

    upload : python test.

---
+++ python/bj/(미완료)bj_25206.py
@@ -0,0 +1,35 @@
+# 문제 링크 (주석) : https://www.acmicpc.net/problem/25206
+# 간단한 문제 설명 : 
+# 해결 방법 설명 : 
+# 시간/공간 복잡도 : 
+
+subject = []
+score = []
+grade = []
+agg = 0
+for i in range(20):
+    subject, score, grade = input().split(" ")
+
+for j in grade:
+    if j == "P":
+        score.remove["P"]
+    elif j == "F":
+        agg += 0.0
+    elif j == "D0":
+        agg += 1.0
+    elif j == "D+":
+        agg += 1.5
+    elif j == "C0":
+        agg += 2.0
+    elif j == "C+":
+        agg += 2.5
+    elif j == "B0":
+        agg += 3.0
+    elif j == "B+":
+        agg += 3.5
+    elif j == "A0":
+        agg += 4.0
+    elif j == "A+":
+        agg += 4.5
+
+print(agg/(len(score)-1))
---
+++ python/bj/bj_2501.py
@@ -0,0 +1,25 @@
+# 문제 링크 (주석) : https://www.acmicpc.net/problem/2501
+# 간단한 문제 설명 : 
+#   N의 약수들 중 K번째로 작은 수를 출력하는 문제입니다.
+#   약수가 K개보다 적으면 0을 출력합니다.
+#
+# 해결 방법 설명 : 
+#   - 1부터 N까지 반복하면서 N의 약수를 찾습니다.
+#   - 찾은 약수들을 리스트에 저장합니다.
+#   - K번째 약수를 출력하거나, 약수의 개수가 부족하면 0을 출력합니다.
+#
+# 시간/공간 복잡도 : 
+#   - 시간 복잡도: O(N) (N까지 순회)
+#   - 공간 복잡도: O(N) (최악의 경우 모든 수가 약수)
+
+N, K = map(int, input().split())
+
+divi_list = []
+for i in range(1, N+1):
+    if N % i == 0:
+        divi_list.append(i)
+
+if len(divi_list) < K:
+    print(0)
+else:
+    print(divi_list[K-1])
