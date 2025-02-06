cd9dd003a4389ea2b99b49ae97317940599ab50c
allenkang92 <ksw8841@knou.ac.kr>
2025-02-07 05:27:47 +0900

    upload : python test.

---
+++ python/pg/[미완료]pg_181857.py
@@ -0,0 +1,18 @@
+# 문제 링크 (주석) : https://school.programmers.co.kr/learn/courses/30/lessons/181857
+# 간단한 문제 설명 : 
+# 해결 방법 설명 : 
+# 시간/공간 복잡도 : 
+
+def solution(arr):
+    answer = []
+    if len(arr) % 2 == 0:
+        pass
+    else:
+        len(arr)
+    check_square = []
+    for i in range(0, 9+1):
+       check_square.append(2 ** i - len(arr))
+    for j in range(min(check_square)):
+        arr.append(0)
+    answer = arr
+    return answer
