1613aa74a5c45b0fe50b80d73ae612eab040c9c2
allenkang92 <ksw8841@knou.ac.kr>
2025-02-06 21:47:52 +0900

    upload : python test.

---
+++ python/pg/pg_12954.py
@@ -0,0 +1,15 @@
+# 문제 링크 (주석) : https://school.programmers.co.kr/learn/courses/30/lessons/12954
+# 간단한 문제 설명 : 
+# 해결 방법 설명 : 
+# 시간/공간 복잡도 : 
+def solution(x, n):
+    answer = []
+    if x == 0:
+        answer = [0] * n
+    elif x > 0:
+        for i in range(x, x * (n + 1), x):
+            answer.append(i)
+    else:
+        for i in range(x, x * (n + 1), x):
+            answer.append(i)
+    return answer
