7e099935bf8afe90ad0b3977a9e9a2ded942a232
allenkang92 <ksw8841@knou.ac.kr>
2025-02-06 20:02:34 +0900

    upload : python test.

---
+++ python/pg/pg_12925.py
@@ -0,0 +1,8 @@
+# 문제 링크 (주석) : https://school.programmers.co.kr/learn/courses/30/lessons/12925
+# 간단한 문제 설명 : 문자열을 정수로 변환
+# 해결 방법 설명 : int() 함수 사용
+# 시간/공간 복잡도 : O(1)
+
+def solution(s):
+    answer = int(s)
+    return answer
