f6f2b1002077cda813af8ff36dff8dd5585e821d
allenkang92 <ksw8841@knou.ac.kr>
2025-02-08 02:12:44 +0900

    upload : python test.

--- python/pg/pg_181861.py
+++ python/pg/pg_181861.py
@@ -1,7 +1,7 @@
 # 문제 링크 (주석) : https://school.programmers.co.kr/learn/courses/30/lessons/181861
-# 간단한 문제 설명 : 
-# 해결 방법 설명 : 
-# 시간/공간 복잡도 : 
+# 간단한 문제 설명 : 배열 arr의 각 원소 a에 대해, 결과 배열의 맨 뒤에 a를 a번 추가하는 작업을 반복합니다.
+# 해결 방법 설명 : 이중 for문을 사용하여 arr의 각 원소(i)에 대해 i번 반복하며 answer 리스트에 i를 추가합니다.
+# 시간/공간 복잡도 : O(n*m) (n: arr의 길이, m: arr의 원소들의 평균값)
 
 def solution(arr):
     answer = []
