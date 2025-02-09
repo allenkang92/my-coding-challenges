f84904e96ebe76024f260cb513674c46eca86a87
allenkang92 <ksw8841@knou.ac.kr>
2025-02-09 18:21:52 +0900

    upload : python test.

---
+++ python/pg/[미완료]pg_120890.py
@@ -0,0 +1,13 @@
+# 문제 링크 (주석) : https://school.programmers.co.kr/learn/courses/30/lessons/120890
+# 간단한 문제 설명 : 
+# 해결 방법 설명 : 
+# 시간/공간 복잡도 : 
+
+def solution(array, n):
+    answer = 0
+    find_n = []
+    for x in array:
+        find_n.append(abs(x - n))
+    min_x = min(find_n)
+    idx = find_n.index(min_x)
+    return array[idx]
---
+++ python/pg/pg_120848.py
@@ -0,0 +1,15 @@
+# 문제 링크 (주석) : https://school.programmers.co.kr/learn/courses/30/lessons/120848
+# 간단한 문제 설명 : 주어진 수 n보다 작거나 같은 가장 큰 팩토리얼 i!의 i를 찾습니다.
+# 해결 방법 설명 : 1부터 시작하여 팩토리얼을 계산하고, 
+#                n보다 커지면 이전 숫자를 반환합니다.
+# 시간/공간 복잡도 : O(n) (n까지의 팩토리얼 계산)
+
+def solution(n):
+    factorial = 1
+    i = 1
+    
+    while factorial <= n:
+        i += 1
+        factorial *= i
+    
+    return i - 1
