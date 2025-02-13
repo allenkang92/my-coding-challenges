bdc7bb7b760e485ffd17910af7256fbe120467ce
allenkang92 <ksw8841@knou.ac.kr>
2025-02-13 22:50:12 +0900

    upload : python test.

---
+++ python/bj/bj_10170.py
@@ -0,0 +1,19 @@
+# 문제 링크 (주석) : https://www.acmicpc.net/problem/10170
+# 간단한 문제 설명 : NFC 서부와 북부 디비전 순위를 출력하는 프로그램을 작성하시오.
+# 해결 방법 설명 : 주어진 ASCII 아트를 그대로 출력
+# 시간/공간 복잡도 : O(1)
+
+print('''NFC West       W   L  T
+-----------------------
+Seattle        13  3  0
+San Francisco  12  4  0
+Arizona        10  6  0
+St. Louis      7   9  0
+
+NFC North      W   L  T
+-----------------------
+Green Bay      8   7  1
+Chicago        8   8  0
+Detroit        7   9  0
+Minnesota      5  10  1
+''')
---
+++ python/bj/bj_5522.py
@@ -0,0 +1,12 @@
+# 문제 링크 (주석) : https://www.acmicpc.net/problem/5522
+# 간단한 문제 설명 : 
+# 해결 방법 설명 :            
+# 시간/공간 복잡도 : 
+
+total_score = 0
+
+for _ in range(5):
+    A = int(input())
+    total_score += A
+
+print(total_score)
