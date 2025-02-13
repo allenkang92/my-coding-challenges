53b03999651474f27edf499fdd8d1848e0bb4946
allenkang92 <ksw8841@knou.ac.kr>
2025-02-13 16:09:01 +0900

    upload : python test.

---
+++ python/cu/cu_6003.py
@@ -0,0 +1,12 @@
+# 문제 링크 (주석) : https://codeup.kr/problem.php?id=6003
+# 간단한 문제 설명 : print() 함수를 사용하여 "Hello"와 "World"를 두 줄로 출력합니다.
+# 해결 방법 설명 : 1. 두 개의 print() 함수를 사용하는 방법
+#                2. \n 이스케이프 문자를 사용하는 방법
+# 시간/공간 복잡도 : O(1) (단순 출력 연산)
+
+# 방법 1: 두 개의 print() 함수 사용
+print("Hello")
+print("World")
+
+# 방법 2: \n 이스케이프 문자 사용
+# print("Hello\nWorld")
