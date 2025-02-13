93632305018226c2235828fdbcb61e078bd1741b
allenkang92 <ksw8841@knou.ac.kr>
2025-02-14 03:39:14 +0900

    upload : python test.

---
+++ python/bj/bj_21598.py
@@ -0,0 +1,9 @@
+# 문제 링크 (주석) : https://www.acmicpc.net/problem/21598
+# 간단한 문제 설명 : 주어진 정수 N만큼 "SciComLove"를 출력하는 문제
+# 해결 방법 설명 : 입력받은 N만큼 반복하여 "SciComLove"를 출력
+# 시간/공간 복잡도 : O(N)
+
+N = int(input())
+
+for _ in range(N):
+    print("SciComLove")
