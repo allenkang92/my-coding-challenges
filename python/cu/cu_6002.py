6f6952af330212707442e8e5a5727cf6da09ccf3
allenkang92 <ksw8841@knou.ac.kr>
2025-02-13 16:05:02 +0900

    upload : python test.

---
+++ python/cu/cu_6002.py
@@ -0,0 +1,8 @@
+# 문제 링크 (주석) : https://codeup.kr/problem.php?id=6002
+# 간단한 문제 설명 : print() 함수를 사용하여 공백이 포함된 "Hello World"를 출력합니다.
+# 해결 방법 설명 : 1. 하나의 문자열로 "Hello World"를 출력하는 방법
+#                2. 두 문자열 "Hello"와 "World"를 공백으로 구분하여 출력하는 방법
+# 시간/공간 복잡도 : O(1) (단순 출력 연산)
+
+print("Hello World")  # 방법 1: 하나의 문자열로 출력
+# print("Hello", "World")  # 방법 2: 두 문자열을 공백으로 구분하여 출력
