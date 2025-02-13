fabb868d79f1299b1b0553c30cf06ee712d4df33
allenkang92 <ksw8841@knou.ac.kr>
2025-02-13 22:44:30 +0900

    upload : python test.

---
+++ python/bj/bj_9654.py
@@ -0,0 +1,12 @@
+# 문제 링크 (주석) : https://www.acmicpc.net/problem/9654
+# 간단한 문제 설명 : 나부 행성의 함대 정보를 출력하는 프로그램을 작성하시오.
+# 해결 방법 설명 : 주어진 ASCII 아트를 그대로 출력
+# 시간/공간 복잡도 : O(1)
+
+print('''SHIP NAME      CLASS          DEPLOYMENT IN SERVICE
+N2 Bomber      Heavy Fighter  Limited    21        
+J-Type 327     Light Combat   Unlimited  1         
+NX Cruiser     Medium Fighter Limited    18        
+N1 Starfighter Medium Fighter Unlimited  25        
+Royal Cruiser  Light Combat   Limited    4         
+''')
