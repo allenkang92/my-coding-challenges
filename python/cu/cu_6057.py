50229dc80011991aaee0cfff7978161f01b86de1
allenkang92 <ksw8841@knou.ac.kr>
2025-02-13 20:35:51 +0900

    upload : python test.

--- python/cu/cu_6057.py
+++ python/cu/cu_6057.py
@@ -1,14 +1,10 @@
 # 문제 링크 (주석) : https://codeup.kr/problem.php?id=6057
-# 간단한 문제 설명 : 
-# 해결 방법 설명 :            
-# 시간/공간 복잡도 : 
+# 간단한 문제 설명 : 두 정수를 입력받아 그 불 값이 서로 같을 때에만 True 를 출력합니다.
+# 해결 방법 설명 : 1. input().split()으로 두 수를 분리합니다.
+#                2. map(int, ...)를 사용하여 각 수를 정수로 변환합니다.
+#                3. 두 값의 bool 값을 비교하여 서로 같을 경우 True를 출력합니다.
+# 시간/공간 복잡도 : O(1) (단순 입출력 및 비교 연산)
 
-a, b = map(int, input().split(" "))
+a, b = map(int, input().split())
 
-if bool(a) == bool(b):
-    print(True)
-else: 
-    print(False)
-
-    2개의 정수값이 입력될 때,
-그 불 값(True/False) 이 서로 같을 때에만 True 를 출력하는 프로그램을 작성해보자.
+print(bool(a) == bool(b))
