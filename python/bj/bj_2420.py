5c561a3b1e1cf1dc8bfa9782f1a84233044c1b51
allenkang92 <ksw8841@knou.ac.kr>
2025-02-13 20:43:38 +0900

    upload : python test.

---
+++ python/bj/bj_2420.py
@@ -0,0 +1,8 @@
+# 문제 링크 (주석) : https://www.acmicpc.net/problem/2420
+# 간단한 문제 설명 : 두 도메인의 유명도 차이를 절대값으로 출력
+# 해결 방법 설명 : 두 수의 차의 절대값을 구한다.
+# 시간/공간 복잡도 : O(1)
+
+N, M = map(int, input().split())
+
+print(abs(N-M))
