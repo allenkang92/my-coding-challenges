784813aa522bc3b31c944420b1e87b3c8a9f8a03
allenkang92 <ksw8841@knou.ac.kr>
2025-02-14 01:54:45 +0900

    upload : python test files.

--- python/bj/bj_14928.py
+++ python/bj/bj_14928.py
@@ -1,8 +1,11 @@
 # 문제 링크 (주석) : https://www.acmicpc.net/problem/14928
-# 간단한 문제 설명 : 
-# 해결 방법 설명 :            
-# 시간/공간 복잡도 : 
+# 간단한 문제 설명 : 큰 수를 20000303으로 나눈 나머지를 구하는 프로그램
+# 해결 방법 설명 : 문자열로 입력받아 Horner's method를 이용하여 나머지 계산
+# 시간/공간 복잡도 : O(N), N은 입력 숫자의 자릿수
 
-N = int(input())
+N = input()
+remainder = 0
+for digit in N:
+    remainder = (remainder * 10 + int(digit)) % 20000303
 
-print(N % 20000303)
+print(remainder)
---
+++ python/bj/bj_15232.py
@@ -0,0 +1,10 @@
+# 문제 링크 (주석) : https://www.acmicpc.net/problem/15232
+# 간단한 문제 설명 : 
+# 해결 방법 설명 :            
+# 시간/공간 복잡도 : 
+
+R = int(input())
+C = int(input())
+
+for _ in range(R):
+    print("*" * C)
