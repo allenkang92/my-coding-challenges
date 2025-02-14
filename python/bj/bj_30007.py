dbb7bdb62ddc128e85e736addd8cb61ecd9d12c0
allenkang92 <ksw8841@knou.ac.kr>
2025-02-15 03:24:43 +0900

    upload : python test.

---
+++ python/bj/bj_30007.py
@@ -0,0 +1,11 @@
+# 문제 링크 (주석) : https://www.acmicpc.net/problem/30007
+# 간단한 문제 설명 : 라면 공식에 따라 필요한 물의 양을 계산하는 문제
+# 해결 방법 설명 : 라면 공식 W = A * (X - 1) + B를 사용하여 필요한 물의 양을 계산
+# 시간/공간 복잡도 : O(N)
+
+N = int(input())
+
+for _ in range(N):
+    A, B, X = map(int, input().split(" "))
+    W = (A * (X - 1)) + B
+    print(W)
