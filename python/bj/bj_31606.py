1ef01520f91cf674cef662330459181182317397
allenkang92 <ksw8841@knou.ac.kr>
2025-02-16 02:21:17 +0900

    upload : python test.

---
+++ python/bj/bj_31606.py
@@ -0,0 +1,8 @@
+# 문제 링크 (주석) : https://www.acmicpc.net/problem/31606
+# 간단한 문제 설명 : 링고(X개), 미캉(Y개), 바나나(3개)의 총합을 구하는 문제
+# 해결 방법 설명 : X + Y + 3을 계산
+# 시간/공간 복잡도 : O(1)
+
+X = int(input())
+Y = int(input())
+print(X + Y + 3)
