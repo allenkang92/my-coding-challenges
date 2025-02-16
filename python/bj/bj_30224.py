14be39119553da4a0a471657fe810178eedb1c24
allenkang92 <ksw8841@knou.ac.kr>
2025-02-16 15:35:56 +0900

    upload : python test files.

---
+++ python/bj/bj_30224.py
@@ -0,0 +1,15 @@
+# 문제 링크 (주석) : https://www.acmicpc.net/problem/30224
+# 간단한 문제 설명 : 
+# 해결 방법 설명 :            
+# 시간/공간 복잡도 : 
+
+ 
+ch = input()
+n = int(ch)
+
+
+
+print(0)
+print(1)
+print(2)
+print(3)
