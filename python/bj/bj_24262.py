8738665af0e90f5da125aa9ef2f454e6d6468e31
allenkang92 <ksw8841@knou.ac.kr>
2025-02-16 14:55:21 +0900

    upload : python test.

---
+++ python/bj/bj_24262.py
@@ -0,0 +1,8 @@
+# 문제 링크 (주석) : https://www.acmicpc.net/problem/24262
+# 간단한 문제 설명 : MenOfPassion 알고리즘에서 코드1의 수행 횟수와 다항식으로 나타냈을 때 최고차항의 차수를 출력
+# 해결 방법 설명 : MenOfPassion 알고리즘은 상수 시간 알고리즘이므로 코드1은 항상 1회 수행되며, 최고차항의 차수는 0이다.
+# 시간/공간 복잡도 : O(1)
+
+n = int(input())
+print(1)
+print(0)
---
+++ python/bj/bj_31654.py
@@ -0,0 +1,12 @@
+# 문제 링크 (주석) : https://www.acmicpc.net/problem/31654
+# 간단한 문제 설명 : 
+# 해결 방법 설명 :            
+# 시간/공간 복잡도 : 
+
+A, B, C = map(int, input().split(" "))
+
+if A + B == C:
+    print("correct!")
+
+else:
+    print("wrong!")
