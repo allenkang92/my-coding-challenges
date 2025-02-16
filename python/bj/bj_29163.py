0a29bfdca79da310403fc23e1d9a284f8242dd3e
allenkang92 <ksw8841@knou.ac.kr>
2025-02-16 14:41:49 +0900

    upload : python test.

--- python/bj/bj_29163.py
+++ python/bj/bj_29163.py
@@ -1,18 +1,18 @@
 # 문제 링크 (주석) : https://www.acmicpc.net/problem/29163
-# 간단한 문제 설명 : 
-# 해결 방법 설명 :            
-# 시간/공간 복잡도 : 
+# 간단한 문제 설명 : 주어진 숫자들 중에서 짝수의 개수가 홀수의 개수보다 많으면 "Happy", 그렇지 않으면 "Sad"를 출력
+# 시간/공간 복잡도 : O(n)
 
 N = int(input())
-line = map(int, input().split(" "))
+numbers = list(map(int, input().split(" ")))
 
 even = 0
 odd = 0
-for x in line:
+for x in numbers:
     if x % 2 == 0:
         even += 1
     else:
         odd += 1
+
 if even > odd:
     print("Happy")
 else:
