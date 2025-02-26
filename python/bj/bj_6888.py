8ecc2c77dace93e8c390a98c2fd2580ef5e43aed
allenkang92 <ksw8841@knou.ac.kr>
2025-02-26 12:47:43 +0900

    upload : python test files.

---
+++ python/bj/bj_6888.py
@@ -0,0 +1,12 @@
+# 문제 링크 (주석) : https://www.acmicpc.net/problem/6888
+# 간단한 문제 설명 : 모든 직책이 바뀌는 연도를 찾는 프로그램입니다.
+# 해결 방법 설명 : 주어진 연도 X에서 시작하여 60년 간격으로 연도를 찾아 출력합니다.
+# 시간/공간 복잡도 : O(n)
+
+X = int(input())
+Y = int(input())
+
+# 모든 직책이 바뀌는 연도는 60년 간격으로 발생
+for year in range(X, Y + 1):
+    if (year - X) % 60 == 0:
+        print(f"All positions change in year {year}")
---
+++ python/bj/bj_8558.py
@@ -0,0 +1,16 @@
+# 문제 링크 (주석) : https://www.acmicpc.net/problem/8558
+# 간단한 문제 설명 : 주어진 n에 대해 n!의 일의 자리 숫자를 계산하는 프로그램입니다.
+# 해결 방법 설명 : n이 5 이상이면 0을 출력하고, 그렇지 않으면 n!을 계산하여 일의 자리 숫자를 출력합니다.
+# 시간/공간 복잡도 : O(n)
+
+n = int(input())
+
+if n == 0 or n == 1:
+    print(1)
+elif n >= 5:
+    print(0)
+else:
+    factorial = 1
+    for i in range(1, n + 1):
+        factorial *= i
+    print(factorial % 10)
