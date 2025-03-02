eb48aa308fe5d4984ca46a98ff897af8368cce4b
allenkang92 <ksw8841@knou.ac.kr>
2025-03-02 15:07:47 +0900

    upload : python test files.

--- python/bj/bj_26768.py
+++ python/bj/bj_26768.py
@@ -1,23 +1,22 @@
 # 문제 링크 (주석) : https://www.acmicpc.net/problem/26768
-# 간단한 문제 설명 : 
-# 해결 방법 설명 :            
-# 시간/공간 복잡도 : 
+# 간단한 문제 설명 : 주어진 문자열을 해커 슬랭으로 변환
+# 해결 방법 설명 : 특정 문자를 해당하는 숫자로 변환
+# 시간/공간 복잡도 : O(n) (문자열의 길이에 비례하여 시간과 공간이 소요됨)
 
-import sys
-line = sys.stdin.readline().rstrip()
+# 입력 받기
+text = input().strip()
 
-trans_line = ""
-for ch in line:
-    if ch == "a": 
-        trans_line += "4"
-    elif ch == "e": 
-        trans_line += "3"
-    elif ch == "i": 
-        trans_line += "1"
-    elif ch == "o": 
-        trans_line += "0"
-    elif ch == "s": 
-        trans_line += "5"
-    else:
-        trans_line += ch
-print(trans_line)
+# 문자 변환 규칙 정의
+conversion = {
+    'a': '4',
+    'e': '3',
+    'i': '1',
+    'o': '0',
+    's': '5'
+}
+
+# 변환된 문자열 생성
+hacker_text = ''.join([conversion.get(char, char) for char in text])
+
+# 결과 출력
+print(hacker_text)
