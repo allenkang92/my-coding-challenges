d18cb8ec7a51fdbeb8721e23e5b036f19e509b9d
allenkang92 <ksw8841@knou.ac.kr>
2025-03-05 21:29:47 +0900

    upload : python test.

---
+++ python/bj/bj_28648.py
@@ -0,0 +1,14 @@
+# 문제 링크 (주석) : https://www.acmicpc.net/problem/28648
+# 간단한 문제 설명 : 여러 버스 중에서 가장 빨리 도착할 수 있는 시간을 계산
+# 해결 방법 설명 : 각 버스의 도착 시간을 계산하고, 가장 작은 값을 찾음
+# 시간/공간 복잡도 : O(n) (n은 버스의 수)
+
+# 입력 받기
+n = int(input())
+buses = [tuple(map(int, input().split())) for _ in range(n)]
+
+# 도착 시간 계산
+min_time = min(t + l for t, l in buses)
+
+# 결과 출력
+print(min_time)
---
+++ python/bj/bj_28808.py
@@ -0,0 +1,17 @@
+# 문제 링크 (주석) : https://www.acmicpc.net/problem/28808
+# 간단한 문제 설명 : 프로그래밍 대회 참가자가 각 문제에 대해 제출한 결과를 바탕으로, 몇 개의 문제를 성공적으로 해결했는지 계산
+# 해결 방법 설명 : 각 문제에 대해 첫 번째로 성공한 제출이 있는지 확인하고, 성공한 문제의 수를 계산
+# 시간/공간 복잡도 : O(n * m) (n은 문제의 수, m은 각 문제당 최대 제출 횟수)
+
+# 입력 받기
+n, m = map(int, input().split())
+results = [input().strip() for _ in range(n)]
+
+# 성공한 문제 계산
+solved = 0
+for result in results:
+    if '+' in result:
+        solved += 1
+
+# 결과 출력
+print(solved)
