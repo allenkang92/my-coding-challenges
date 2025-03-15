ba0109786f14e66ebc34c6a1ca1e65eaf4da7b94
allenkang92 <ksw8841@knou.ac.kr>
2025-03-15 20:01:56 +0900

    upload : python test files.

---
+++ python/bj/bj_32089.py
@@ -0,0 +1,25 @@
+# 문제 링크 (주석) : https://www.acmicpc.net/problem/32089
+# 간단한 문제 설명 : n년 동안의 각 해의 신입 부원 수를 기반으로, 부원 수가 최대가 되는 해의 부원 수를 계산
+# 해결 방법 설명 : 각 해의 부원 수는 해당 해와 그 전 2년의 신입 부원 수의 합으로 계산
+# 시간/공간 복잡도 : O(n)
+
+# 입력 처리
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    a = list(map(int, input().split()))
+    
+    # 최대 부원 수 계산
+    max_members = 0
+    for i in range(n):
+        current_members = a[i]
+        if i >= 1:
+            current_members += a[i - 1]
+        if i >= 2:
+            current_members += a[i - 2]
+        if current_members > max_members:
+            max_members = current_members
+    
+    # 결과 출력
+    print(max_members)
---
+++ python/bj/bj_32278.py
@@ -0,0 +1,19 @@
+# 문제 링크 (주석) : https://www.acmicpc.net/problem/32278
+# 간단한 문제 설명 : 주어진 정수 N에 대해, 정확하게 표현할 수 있으면서 메모리를 가장 적게 차지하는 자료형(short, int, long long)을 선택
+# 해결 방법 설명 : N의 값이 각 자료형의 범위 안에 있는지 확인하여 가장 작은 자료형을 선택
+# 시간/공간 복잡도 : O(1)
+
+import sys
+
+# 입력 처리: sys.stdin.readline()을 사용하여 입력을 받고, rstrip()으로 공백 제거
+N = int(sys.stdin.readline().rstrip())
+
+# short 범위: -2^15 <= N <= 2^15 - 1
+if ((-2) ** 15) <= N <= (2 ** 15 - 1):
+    print('short')    
+# int 범위: -2^31 <= N <= 2^31 - 1
+elif ((-2) ** 31) <= N <= (2 ** 31 - 1):
+    print('int')    
+# long long 범위: -2^63 <= N <= 2^63 - 1
+elif ((-2) ** 63) <= N <= (2 ** 63 - 1):
+    print('long long')
