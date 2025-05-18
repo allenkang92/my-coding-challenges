d97a1c77f5838a328bb86d60780a55bf353dd733
allenkang92 <ksw8841@knou.ac.kr>
2025-05-18 15:41:33 +0900

    feat: implement solution for BOJ 33810 problem using Python dictionary and list comprehension.

--- python/bj/bj_33810.py
+++ python/bj/bj_33810.py
@@ -1,19 +1,19 @@
 # 문제 링크 : https://www.acmicpc.net/problem/33810
-# 간단한 문제 설명 : 
-# 해결 방법 설명 :            
-# 시간/공간 복잡도 : 
+# 간단한 문제 설명 : 주어진 문자열이 "SciComLove"와 비교하여 몇 개의 문자가 다른지 계산
+# 해결 방법 설명 : 각 위치별로 문자열을 비교하여 다른 문자의 개수를 세기
+# 시간/공간 복잡도 : O(1) - 문자열의 길이가 항상 10으로 고정되어 있음
 
+# 원본 문자열 정의
+original = "SciComLove"
 
+# 입력 받기
+modified = input().strip()
 
-S = input()
+# 다른 문자 개수 계산
+diff_count = 0
+for i in range(len(original)):
+    if original[i] != modified[i]:
+        diff_count += 1
 
-char = "SciComLove"
-
-count = 0
-for i in range(len(S)):
-    if S[i] != char[i]:
-        count += 1
-    else:
-        count += 0
-
-print(count)
+# 결과 출력
+print(diff_count)
